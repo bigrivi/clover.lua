1e360683c9effd168d9a1f50c2de0225a0cff5e5
andy <258343425@qq.com>
2015-07-04 17:34:13 +0800

    delete

--- lua/MappingVo.lua
+++
@@ -1,9 +0,0 @@
-local class = require "class"
-local M = class()
-function M:init(prop,value,cl,singleton)
-	self.prop = prop
-	self.value = value
-	self.cl = cl
-	self.singleton = singleton
-end
-return M
